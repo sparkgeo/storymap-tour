ac104cf613b1cdaa5d0348c0baf4052c0cbc078c
Darren Wiens <dkwiens@gmail.com>
2017-07-05 15:29:19 -0700

    'demo-ready' topic launches demo

--- MapTour/src/app/storymaps/maptour/core/Maptiks.js
+++ MapTour/src/app/storymaps/maptour/core/Maptiks.js
@@ -1,20 +1,16 @@
 define(["dojo/topic"],
   function(topic){
     topic.subscribe("maptour-ready", function(){
-      require(['maptiks'], function (mapWrapper) {
-        if (!app.map.maptiks) {
+      require(["maptiks"], function (mapWrapper) {
+        if (app.data.getWebAppData().values.maptiks) {
           var container = app.map.container; // the current map div
-          var maptiksSet = app.data.getWebAppData().values.hasOwnProperty('maptiks');
-          var maptiksTrackcode = maptiksSet ? app.data.getWebAppData().values.maptiks.maptiksTrackcode : 'c311cf16-ad79-42b1-97f9-f433be6c8b00'; // replace with dev trackcode
-          var maptiksId = maptiksSet ? app.data.getWebAppData().values.maptiks.maptiksId : 'demo';
           var maptiksMapOptions = {
-            extent: app.map.extent,
-            maptiks_trackcode: maptiksTrackcode,
-            maptiks_id: maptiksId + ":" + app.map.id
+            maptiks_trackcode: app.data.getWebAppData().values.maptiks.maptiksTrackcode, // from Builder Maptiks settings
+            maptiks_id: app.data.getWebAppData().values.maptiks.maptiksId + ":" + app.map.id // from Builder Maptiks settings, ID:tabname
           };
           mapWrapper(container, maptiksMapOptions, app.map);
-          topic.publish("maptiks-ready");
         }
+        topic.publish("demo-ready", mapWrapper);
       });
     });
   }
