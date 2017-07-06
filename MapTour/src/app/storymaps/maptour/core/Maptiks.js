0960b772a275bc5e97b6911ec78cf6c6639f7931
Darren Wiens <dkwiens@gmail.com>
2017-07-06 10:23:31 -0700

    dev trackcode

--- MapTour/src/app/storymaps/maptour/core/Maptiks.js
+++ MapTour/src/app/storymaps/maptour/core/Maptiks.js
@@ -2,7 +2,7 @@ define(["dojo/topic"],
   function(topic){
     topic.subscribe("maptour-ready", function(){
       require(["maptiks"], function (mapWrapper) {
-        if (app.data.getWebAppData().values.maptiks) {
+        if (app.data.getWebAppData().values.maptiks) { // if maptiks parameters entered in builder
           var container = app.map.container; // the current map div
           var maptiksMapOptions = {
             maptiks_trackcode: app.data.getWebAppData().values.maptiks.maptiksTrackcode, // from Builder Maptiks settings
